f1bcbf20f011ca6b077c6a672d23f81d0eb65c6a
Lenalight <lena.lux00@gmail.com>
2023-10-19 14:07:17 -0700

    singly_linked_lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 
-/* Function marked as a constructor to print messages before main */
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - Prints the messages "You're beat! and yet, you must allow,"
- * and "I bore my house upon my back!" before main.
+ * first - Prints a sentence before the main function is executed
  */
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I carry my home upon my back!\n");
-}
-
-int main(void)
-{
-    /* Rest of your main function code */
-
-	return (0);
+	printf("I bore my house upon my back!\n");
 }
 
